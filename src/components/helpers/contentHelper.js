9727bf5ad1e311284c1bddbc477607b53b8512ec
Christoffer Grännby <cribbster1987@hotmail.com>
2022-01-05 15:11:16 +0100

    minor css fixes

--- src/components/helpers/contentHelper.js
+++ src/components/helpers/contentHelper.js
@@ -4,16 +4,8 @@ import Rating from '../Rating';
 
 export const getContent = (imdbId, type) => {
   return request.getContent(`/${type}/${imdbId}`);
-};
+}
 
-<<<<<<< HEAD
-export const renderContent = (content) => {
-  console.log(content);
-  return <div className='test'>HEEEj</div>;
-};
-
-=======
->>>>>>> fe66605ae5eedf1ef580770f61e9f37cea31d0e5
 export const getTopTen = (type) => {
   return request.getContent(`/${type}/topTen`)
 }
