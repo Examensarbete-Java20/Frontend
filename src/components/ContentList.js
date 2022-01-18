284bda6d2e97067158903f3c5e3b854cec1b3b71
yadonx <ej_yo@hotmail.com>
2022-01-18 09:58:31 +0100

    added an alt img if content dont have a poster¨

--- src/components/ContentList.js
+++ src/components/ContentList.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import notFoundImg from '../styles/img/noimagefound.jpg';
 import Rating from './Rating';
 
 const ContentList = ({ content, type, contentAction }) => {
-  console.log(content);
   return content.map((result) => (
     <div key={result.imdb_id} className='card'>
       <div className='imageWrapper'>
@@ -13,7 +13,15 @@ const ContentList = ({ content, type, contentAction }) => {
           className='linkGrid'
           to={`/show/${type}/${result.imdb_id}`}
         >
-          <img src={result.image_url} className='image' alt='No Img' />
+          <img
+            src={result.image_url}
+            className='image'
+            alt='No Img'
+            onError={(e) => {
+              e.target.onerror = null;
+              e.target.src = notFoundImg;
+            }}
+          />
           <h3 className='title'>
             {result.title}
             {result.release && ` (${result.release.substring(0, 4)})`}
